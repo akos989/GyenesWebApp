ec40902908aed4ab7506068ba2bb881e1e82659a
akos989 <morvaiakos1998@gmail.com>
2020-04-03 20:26:23 +0200

    hamburger to arrow

--- src/app/client/header/header.component.ts
+++ src/app/client/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  navigationOpen = false;
 
+  constructor() { }
+  
   ngOnInit(): void {
   }
 
+  navigatonClicked() {
+    this.navigationOpen = !this.navigationOpen;
+  }
+ 
 }
