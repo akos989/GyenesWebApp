afdf328964ff31f8d2840c747a2fbd01df308cb1
akos989 <morvaiakos1998@gmail.com>
2020-04-03 14:20:26 +0200

    add bootstrap and ngx-facebook

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FacebookModule } from 'ngx-facebook';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
