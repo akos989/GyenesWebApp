48cbb92c5f86f2917dd0065e34b61619181db3ec
akos989 <morvaiakos1998@gmail.com>
2020-04-06 13:05:22 +0200

    png color filter

--- src/app/client/header/header.component.ts
+++ src/app/client/header/header.component.ts
@@ -1,8 +1,5 @@
 import { Component, OnInit, ComponentFactoryResolver, ViewChild } from '@angular/core';
 
-import { NavigationComponent } from '../navigation/navigation.component';
-import { PlaceholderDirective } from 'src/app/shared/placeholder/placeholder.directive';
-
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -13,9 +10,8 @@ export class HeaderComponent implements OnInit {
   navigationOpen = false;
   small = false;
   smallBeforeNav:boolean = null;
-  @ViewChild(PlaceholderDirective, {static: false}) navigationHost: PlaceholderDirective; 
 
-  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
+  constructor() { }
   
   ngOnInit(): void {
   }
@@ -25,33 +21,14 @@ export class HeaderComponent implements OnInit {
       this.smallBeforeNav = this.small;
     this.navigationOpen = !this.navigationOpen;
     if (this.navigationOpen) {
-      //this.showNavigationArea();
       this.small = false;
     } else {
-      //this.closeNavigationArea();
       if (this.smallBeforeNav !== null)
         this.small = this.smallBeforeNav;
-    }
-      
+    }      
   }
  
   toggleSmallClicked() {
     this.small = !this.small;
-  }
-
-  private showNavigationArea() {
-    const navigationCmpFactory =
-      this.componentFactoryResolver.resolveComponentFactory(NavigationComponent);
-    const hostViewContainerRef = this.navigationHost.viewContainerRef;
-    hostViewContainerRef.clear();
-
-    const componentRef = hostViewContainerRef.createComponent(navigationCmpFactory);
-    
-    componentRef.instance.openClicked();
-  }
-
-  private closeNavigationArea() {
-    const hostViewContainerRef = this.navigationHost.viewContainerRef;
-    hostViewContainerRef.clear();
   }
 }
