ad2d22d84c5e4e818da8970be627baa6eea41be5
akos989 <morvaiakos1998@gmail.com>
2020-04-06 12:22:43 +0200

    navigation page

--- src/app/client/header/header.component.ts
+++ src/app/client/header/header.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { Component, OnInit, ComponentFactoryResolver, ViewChild } from '@angular/core';
+
+import { NavigationComponent } from '../navigation/navigation.component';
+import { PlaceholderDirective } from 'src/app/shared/placeholder/placeholder.directive';
 
 @Component({
   selector: 'app-header',
@@ -10,17 +12,46 @@ export class HeaderComponent implements OnInit {
 
   navigationOpen = false;
   small = false;
+  smallBeforeNav:boolean = null;
+  @ViewChild(PlaceholderDirective, {static: false}) navigationHost: PlaceholderDirective; 
 
-  constructor() { }
+  constructor(private componentFactoryResolver: ComponentFactoryResolver) { }
   
   ngOnInit(): void {
   }
 
   navigatonClicked() {
+    if (!this.navigationOpen)
+      this.smallBeforeNav = this.small;
     this.navigationOpen = !this.navigationOpen;
+    if (this.navigationOpen) {
+      //this.showNavigationArea();
+      this.small = false;
+    } else {
+      //this.closeNavigationArea();
+      if (this.smallBeforeNav !== null)
+        this.small = this.smallBeforeNav;
+    }
+      
   }
  
   toggleSmallClicked() {
     this.small = !this.small;
+  }
+
+  private showNavigationArea() {
+    const navigationCmpFactory =
+      this.componentFactoryResolver.resolveComponentFactory(NavigationComponent);
+    const hostViewContainerRef = this.navigationHost.viewContainerRef;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(navigationCmpFactory);
+    
+    componentRef.instance.openClicked();
+  }
+
+  private closeNavigationArea() {
+    const hostViewContainerRef = this.navigationHost.viewContainerRef;
+    hostViewContainerRef.clear();
   }
 }
--- src/app/client/navigation/navigation.component.ts
+++ src/app/client/navigation/navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-navigation',
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  @Input() open:boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.open = false;
   }
 
+  openClicked() {
+    this.open = !this.open;
+    console.log(this.open);
+  }
 }
