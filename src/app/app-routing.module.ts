bd112a7d249e57b7363afbd0ea3d574d052aabd7
akos989 <morvaiakos1998@gmail.com>
2020-04-06 14:57:04 +0200

    header small based on location

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { HomeComponent } from './client/home/home.component';
+import { PricesComponent } from './client/prices/prices.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'prices', component: PricesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/client/header/header.component.ts
+++ src/app/client/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ComponentFactoryResolver, ViewChild } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -8,21 +9,31 @@ import { Component, OnInit, ComponentFactoryResolver, ViewChild } from '@angular
 export class HeaderComponent implements OnInit {
 
   navigationOpen = false;
-  small = false;
+  small: boolean;
   smallBeforeNav:boolean = null;
 
-  constructor() { }
+  constructor(private location: Location, private renderer: Renderer2) { }
   
   ngOnInit(): void {
+    console.log(this.location.path())
+    if ( this.location.path() === '' || this.location.path() === '/home' ) {
+      this.small = false;
+    } else {
+      this.small = true;
+    }
+    
   }
 
   navigatonClicked() {
     if (!this.navigationOpen)
       this.smallBeforeNav = this.small;
     this.navigationOpen = !this.navigationOpen;
+
     if (this.navigationOpen) {
+      this.renderer.addClass(document.body, 'modal-open');
       this.small = false;
     } else {
+      this.renderer.removeClass(document.body, 'modal-open');
       if (this.smallBeforeNav !== null)
         this.small = this.smallBeforeNav;
     }      
