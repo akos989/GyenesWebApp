18c036f205006ec09d31c57bb9fceae40f84a135
akos989 <morvaiakos1998@gmail.com>
2020-04-20 21:36:47 +0200

    image slider with ngx-swipe

--- src/app/client/home/slider/slider.component.ts
+++ src/app/client/home/slider/slider.component.ts
@@ -1,15 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { SwiperConfigInterface,
+  SwiperScrollbarInterface, SwiperPaginationInterface } from 'ngx-swiper-wrapper';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
-  styleUrls: ['./slider.component.css']
+  styleUrls: [ './slider.component.css' ]
 })
-export class SliderComponent implements OnInit {
+export class SliderComponent {
 
-  constructor() { }
+  public show: boolean = true;
 
-  ngOnInit(): void {
-  }
+  public slides = [
+    'First slide',
+    'Second slide',
+    'Third slide',
+    'Fourth slide',
+    'Fifth slide',
+    'Sixth slide'
+  ];
+
+  public config: SwiperConfigInterface = {
+    a11y: true,
+    effect: 'coverflow',
+    grabCursor: true,
+    centeredSlides: true,
+    direction: 'horizontal',
+    breakpoints: {
+      640: {
+        slidesPerView: 1
+      },
+      768: {
+        slidesPerView: 2
+      },
+      1024: {
+        slidesPerView: 3
+      },
+    },
+    keyboard: true,
+    mousewheel: true,
+    scrollbar: false,
+    navigation: true,
+    pagination: true,
+    coverflowEffect: {
+      rotate: 50,
+      stretch: 0,
+      depth: 100,
+      modifier: 1,
+      slideShadows : true,
+    }
+  };
+
+  constructor() {}
 
 }
