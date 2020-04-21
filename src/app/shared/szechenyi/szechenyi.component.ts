0084b26bae879649e20f23cd4ecc270b25cd879c
akos989 <morvaiakos1998@gmail.com>
2020-04-21 11:58:07 +0200

    szechenyi and privacy statement

---
+++ src/app/shared/szechenyi/szechenyi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-szechenyi',
+  templateUrl: './szechenyi.component.html',
+  styleUrls: ['./szechenyi.component.css']
+})
+export class SzechenyiComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
