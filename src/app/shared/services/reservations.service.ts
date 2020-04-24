b10233c7f3941961f38ee60ed9444e37444c3e84
akos989 <morvaiakos1998@gmail.com>
2020-04-24 20:55:57 +0200

    delete from local storage & no reserv for today

--- src/app/shared/services/reservations.service.ts
+++ src/app/shared/services/reservations.service.ts
@@ -97,13 +97,19 @@ export class ReservationService {
     retrieveFromLocalStorage() {
         const currentReservation =
             JSON.parse(localStorage.getItem('currentReservation'));
-        this._currentReservation = currentReservation;
-        const dateParts:string[] = currentReservation.date.split('T');
-        this._currentReservation.date = new Date(
-            +(dateParts[0].split('-')[0]),
-            +(dateParts[0].split('-')[1]) - 1,
-            +(dateParts[0].split('-')[2]),
-            +(dateParts[1].split(':')[0]) + 2       
-        );
+        if (currentReservation !== null && currentReservation !== "") {
+            this._currentReservation = currentReservation;
+            const dateParts:string[] = currentReservation.date.split('T');
+            this._currentReservation.date = new Date(
+                +(dateParts[0].split('-')[0]),
+                +(dateParts[0].split('-')[1]) - 1,
+                +(dateParts[0].split('-')[2]),
+                +(dateParts[1].split(':')[0]) + 2       
+            );
+            if ( this._currentReservation.date.valueOf() <= new Date().valueOf() ) {
+                this._currentReservation = null;
+                localStorage.removeItem("currentReservation");
+            }
+        }        
     }
 }
