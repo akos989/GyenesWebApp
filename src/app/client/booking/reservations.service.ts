67f6d716c585a22d2bc2b7dfc825f17682282d86
akos989 <morvaiakos1998@gmail.com>
2020-04-26 01:30:29 +0200

    reservations intersect (start + duration)

--- src/app/client/booking/reservations.service.ts
+++ src/app/client/booking/reservations.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@angular/core';
 import { Reservation } from '../../shared/models/reservation.model';
 import { Subject } from 'rxjs/internal/Subject';
+import { PackageService } from '../packages/package.service';
 
 class Hour {
     constructor(public hour: number, public type: string, public remainingNumber: number) {}
 }
 
+class Range {
+    constructor (public start:number, public end: number) {}
+}
+
 @Injectable({providedIn: 'root'})
 export class ReservationService {
+
+    constructor(private packageService: PackageService) {}
 
     private _currentReservation: Reservation = null;   
 
@@ -33,19 +40,19 @@ export class ReservationService {
         //szerver kérés
         this.reservations = [
             new Reservation(
-                '1', 'morvai', 'adf@asdf.com', '0123457898', 12, '', 'as2',
+                '1', 'morvai', 'adf@asdf.com', '0123457898', 12, '', '2',
                 new Date(2020, 4, 11, 12)
             ),
             new Reservation(
-                '2', 'morvai', 'adf@asdf.com', '0123457898', 25, '', 'as2',
+                '2', 'morvai', 'adf@asdf.com', '0123457898', 25, '', '3',
                 new Date(2020, 4, 11, 12)
             ),
             new Reservation(
-                '3', 'morvai', 'adf@asdf.com', '0123457898', 10, '', 'as2',
+                '3', 'morvai', 'adf@asdf.com', '0123457898', 10, '', '2',
                 new Date(2020, 4, 11, 10)
             ),
             new Reservation(
-                '4', 'morvai', 'adf@asdf.com', '0123457898', 17, '', 'as2',
+                '4', 'morvai', 'adf@asdf.com', '0123457898', 17, '', '2',
                 new Date(2020, 4, 11, 16)
             )
             ];
@@ -57,7 +64,10 @@ export class ReservationService {
             let tmpPlayerNumber: number = this._currentReservation.playerNumber;
             const reservations = this.getReservationsOnSelectedDate(selectedDate);
             for( let reservation of reservations ) {
-                if ( reservation.date.getHours() === hour ) {
+                // if ( reservation.date.getHours() === hour ) {
+                //     tmpPlayerNumber += reservation.playerNumber;
+                // }                
+                if ( this.intersect(hour, reservation) ) {
                     tmpPlayerNumber += reservation.playerNumber;
                 }
             }
@@ -127,6 +137,26 @@ export class ReservationService {
             this._currentReservation.date = null;
             this.saveToLocalStorage();
         }
+    }
+
+    private intersect(hour: number, reservation: Reservation): boolean {
+        const a = new Range(
+            reservation.date.getHours(),
+            reservation.date.getHours() +
+            this.packageService.findById(reservation.packageId).duration
+        );
+        const b = new Range(
+            hour, hour +
+            this.packageService.findById(this.currentReservation.packageId).duration
+        );
+
+        const min = (a.start < b.start) ? a : b;
+        const max = (a === min)? b : a;
+
+        if ( min.end <= max.start )
+            return false;
+        
+        return true;
     }
 
 }
