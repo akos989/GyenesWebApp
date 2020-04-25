d2fdc7fa7efcb105e95837731a37e11151e4c9b7
akos989 <morvaiakos1998@gmail.com>
2020-04-26 01:30:29 +0200

    reservations intersect (start + duration)

--- src/app/client/packages/package.service.ts
+++ src/app/client/packages/package.service.ts
@@ -11,7 +11,7 @@ export class PackageService {
         this.packages = [
             new Package('1', 'Kicsi', 3, 10, 15, 3000, 2, false, 0),
             new Package('2', 'Közepes', 11, 20, 12, 3000, 2, false, 0),
-            new Package('3', 'Nagy', 21, 35, 10, 3000, 2, false, 0),
+            new Package('3', 'Nagy', 21, 35, 10, 3000, 4, false, 0),
             new Package('4', 'Akció1', 15, 20, 15, 5000, 2, true, 300),
             new Package('5', 'Akció2', 25, 35, 13, 6000, 2, true, 500),
         ];
@@ -19,10 +19,10 @@ export class PackageService {
 
     findById(id: string): Package {
         this.loadFromBackend();
-       for (const p of this.packages) {
+        for (const p of this.packages) {
             if (p.id === id)
                 return p;
-       }
-       return null;
+        }
+        return null;
     }
 }
