f3c748698e996842a190e16b08ea419dad4d685b
akos989 <morvaiakos1998@gmail.com>
2020-04-26 10:56:14 +0200

    reason when closed date

--- src/app/client/booking/date/no-dates.service.ts
+++ src/app/client/booking/date/no-dates.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 
 class NoDate {
-    constructor(public fromDate: Date, public toDate: Date) {}
+    constructor(public fromDate: Date, public toDate: Date, public reason: string) {}
 }
 
 @Injectable({providedIn: 'root'})
@@ -11,18 +11,18 @@ export class NoDatesService {
     loadNoDatesForMonth(date: Date) {
         //server http
         this.noDates = [
-            new NoDate(new Date(2020, 4, 1), new Date(2020, 4, 7)),
-            new NoDate(new Date(2020, 4, 21), new Date(2020, 11, 23))
+            new NoDate(new Date(2020, 4, 1), new Date(2020, 4, 7), 'sdf'),
+            new NoDate(new Date(2020, 4, 21), new Date(2020, 11, 23), 'COVID19')
         ];
     }
 
-    isClosed(date: Date): boolean {
+    isClosed(date: Date): string {
         for( const nd of this.noDates ) {
             if (nd.fromDate.valueOf() <= date.valueOf() &&
                 date.valueOf() < nd.toDate.valueOf()
             )
-                return true;
+                return nd.reason;
         }
-        return false;
+        return '';
     }
 }
