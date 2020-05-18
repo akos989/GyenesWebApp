aa4ac13fa19bcda50da918d50a23b824297a239d
akos989 <morvaiakos1998@gmail.com>
2020-05-18 11:44:06 +0200

    landing page scroll down sign

--- src/app/client/home/landing/landing.component.ts
+++ src/app/client/home/landing/landing.component.ts
@@ -1,15 +1,54 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
+import { state, trigger, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-landing',
   templateUrl: './landing.component.html',
-  styleUrls: ['./landing.component.css']
+  styleUrls: ['./landing.component.css'],
+  animations: [
+    trigger('buttonState', [
+      state('in', style({
+        opacity: 1
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0
+        }),
+        animate(500)
+      ]),
+      transition('* => void', [
+        animate(500, style({
+          opacity: 0
+        }))
+      ])
+    ])
+  ]
 })
 export class LandingComponent implements OnInit {
 
-  constructor() { }
+  displayDown: boolean = false;
+  scrolled: boolean = false;
+
+  constructor(private renderer: Renderer2) { }
 
   ngOnInit(): void {
+    if (!this.scrolled) {
+      this.renderer.listen(window, 'scroll', () => {
+        this.scrolled = true;
+        this.displayDown = false;
+      });
+    }
+    setTimeout(() => {
+      if (!this.scrolled)
+        this.displayDown = true;
+    }, 5000);
   }
 
+  scrollDown() {
+    const vh = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+    window.scrollTo({
+      top: vh,
+      behavior: 'smooth'
+    });
+  }
 }
