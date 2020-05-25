6581e87b0077a9d7799c8ff3a9aa521788e5a194
akos989 <morvaiakos1998@gmail.com>
2020-05-25 15:18:32 +0200

    game-modes

--- src/app/client/game-modes/game-modes.component.ts
+++ src/app/client/game-modes/game-modes.component.ts
@@ -1,15 +1,71 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit, ElementRef, Renderer2, ViewChildren, QueryList } from '@angular/core';
+
+class GameMode {
+  constructor(public name: string, public description: string, public imgUrl1, public imgUrl2 = '') {}
+}
 
 @Component({
   selector: 'app-game-modes',
   templateUrl: './game-modes.component.html',
   styleUrls: ['./game-modes.component.css']
 })
-export class GameModesComponent implements OnInit {
+export class GameModesComponent implements AfterViewInit {
 
-  constructor() { }
+  extended = true;
+  scrolled = false;
 
-  ngOnInit(): void {
+  gameModeArray: GameMode[] = [
+    new GameMode('Várfoglalás', 'lakjsdf', "../../../assets/pictures/paintball.jpg", "../../../assets/pictures/paintball.jpg"),
+    new GameMode('Capture the flag', 'lakjsdf', "../../../assets/pictures/paintball.jpg"),
+    new GameMode('Speedball', 'lakjsdf', "../../../assets/pictures/paintball.jpg", "../../../assets/pictures/paintball.jpg"),
+    new GameMode('TDM', 'lakasdasdasdasdasdasd asd asd asd asdasdas djsdf', "../../../assets/pictures/paintball.jpg"),
+    new GameMode('Capture the point', 'lakjsdf', "../../../assets/pictures/paintball.jpg")
+  ];
+
+  @ViewChildren('gameModes') gameModeElements: QueryList<ElementRef>;
+
+  constructor(private renderer: Renderer2) { }
+
+  ngAfterViewInit() {
+    const toggleOffset = window.innerHeight * 0.4;
+    this.renderer.listen(window, 'scroll', () => {
+      const windowOffset = window.scrollY;
+      if (!this.scrolled) {
+        if (windowOffset > toggleOffset + toggleOffset * 0.05) {
+          this.extended = false;
+          this.scrolled = true;
+        }
+      } else {
+        if (windowOffset < 10) {
+          this.extended = true;
+          this.scrolled = false;
+        }
+      }
+    });
+  }
+
+  scrollToElement(idx: number) {
+    this.extended = false;
+    let positon: number = this.gameModeElements.toArray()[idx].nativeElement.offsetTop;
+    positon -= window.innerHeight * 0.6;
+    const offset = positon - window.pageYOffset;
+    const addition = (offset > 0 ? 10 : -10);
+    if (addition < 0) {
+      positon -= window.innerHeight * 0.2;
+    }
+    let scrollToTop = window.setInterval(() => {
+      let pos = window.scrollY;
+      if (!((pos < positon) && (pos > positon - 100))) {
+          window.scrollTo(0, pos + addition);
+          pos += addition;
+      } else {
+          window.clearInterval(scrollToTop);
+      }
+    }, 4);
+  }
+
+  extend() {
+    this.extended= !this.extended;
   }
 
 }
