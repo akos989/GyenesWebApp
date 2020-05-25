c4ca8e0c537321ebd5a4078d51ff882e9422b645
akos989 <morvaiakos1998@gmail.com>
2020-05-25 15:18:32 +0200

    game-modes

--- src/app/client/game-modes/game-modes-details/game-modes-details.component.ts
+++ src/app/client/game-modes/game-modes-details/game-modes-details.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-game-modes-details',
   templateUrl: './game-modes-details.component.html',
   styleUrls: ['./game-modes-details.component.css']
 })
-export class GameModesDetailsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class GameModesDetailsComponent {
+ 
 
 }
