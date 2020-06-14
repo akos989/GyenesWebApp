565825516d82a1d595beb1ff1068372729800dae
akos989 <morvaiakos1998@gmail.com>
2020-06-14 15:21:19 +0200

    no dates component generation

---
+++ src/app/operators/no-dates/no-date-detail/no-date-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-date-detail',
+  templateUrl: './no-date-detail.component.html',
+  styleUrls: ['./no-date-detail.component.css']
+})
+export class NoDateDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/operators/no-dates/no-date-new/no-date-new.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-no-date-new',
+  templateUrl: './no-date-new.component.html',
+  styleUrls: ['./no-date-new.component.css']
+})
+export class NoDateNewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
